3d1ef4beede42c08e3551eebde8b61b1ae22481c
Vinicius Gomes <Vinicius000Fb@gmail.com>
2021-09-06 18:44:04 -0300

    Rewriting code | 0.0.2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    name: 'Vinicius Gomez',
-    counter: 0 
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        corpo: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        corpo: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        corpo: 'O corpo 3'
+      },
+    ]
   };
 
   render() {
