19b561d740a23799a70c46e784a326fadb74a24d
Vinicius Gomes <Vinicius000Fb@gmail.com>
2021-09-06 21:14:10 -0300

    Rewriting code | 0.0.4

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ class App extends React.Component {
       {
         id: 1,
         title: 'O título 1',
-        corpo: 'O corpo 1'
+        body: 'O corpo 1'
       },
       {
         id: 2,
         title: 'O título 2',
-        corpo: 'O corpo 2'
+        body: 'O corpo 2'
       },
       {
         id: 3,
         title: 'O título 3',
-        corpo: 'O corpo 3'
+        body: 'O corpo 3'
       },
     ]
   };
@@ -29,7 +29,12 @@ class App extends React.Component {
     
     return (
       <div className="App">
-        {posts.map(post => <h1>{post.title}</h1>)}
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1> {post.title} </h1>
+            <p> {post.body} </p>
+          </div>
+        ))}
       </div>
     );
   }
